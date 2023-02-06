8dd6922c1b57bc1d28bba5103f72feabe80f73d0
Saurabh Dave <sdsaurabhdave@gmail.com>
2023-02-06 11:00:25 -0600

    Initial Commit

---
+++ ScannerGPT/ScannerGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerGPTApp.swift
+//  ScannerGPT
+//
+//  Created by Saurabh Dave on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ScannerGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
